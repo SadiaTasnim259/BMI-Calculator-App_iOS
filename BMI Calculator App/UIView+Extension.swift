023daa17632908b33e869d52564114ad62345810
Sadia Tasnim <tasnimsadia259@gmail.com>
2023-07-31 11:23:35 +0600

    complete design

---
+++ BMI Calculator App/UIView+Extension.swift
@@ -0,0 +1,40 @@
+//
+//  UIView+Extension.swift
+//  BMI Calculator App
+//
+//  Created by Sadia on 31/7/23.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    
+    @IBInspectable var cornerRadiusV: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+    
+    @IBInspectable var borderWidthV: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable var borderColorV: UIColor? {
+        get {
+            return UIColor(cgColor: layer.borderColor!)
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+}
